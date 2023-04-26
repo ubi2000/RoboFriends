05451685ddbac1817a836984138c844700f7eeca
ubi2000 <103331253+ubi2000@users.noreply.github.com>
2023-04-26 11:49:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const {robots,searchfield}=this.state
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(searchfield.toLowerCase());
+    });
+          return !robots.length ? <h1>Loading please wait!</h1>:<div className="tc ">
+          <h1 style={{fontFamily:"Roboto",color:"red"}}> RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+  }
+}
+
+export default App;
---
+++ Scroll.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return(
+    <div style={{overflowY:"scroll",border: "5px solid red",height:"600px"}}>
+        {props.children}
+    </div>
+  )
+};
+export default Scroll;
